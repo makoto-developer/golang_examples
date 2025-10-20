c07d9f4cea850c63d6c58217db15fba6f4392a2d
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2025-10-20 23:39:07 +0900

    fix sql

--- gorm/gorm/model/Order.go
+++ gorm/gorm/model/Order.go
@@ -8,16 +8,12 @@ import (
 
 type Order struct {
 	ID               int64          `gorm:"primaryKey" json:"id"`
-	OrderItemGroupID int64          `json:"order_item_group_id"`
-	UserID           int64          `json:"user_id" gorm:"index"`
-	Amount           int64          `json:"amount"`
-	AmountWithoutTax int64          `json:"amount_without_tax"`
-	Tax              int64          `json:"tax"`
-	CreatedAt        time.Time      `json:"created_at"`
-	UpdatedAt        time.Time      `json:"updated_at"`
-	DeletedAt        gorm.DeletedAt `json:"deleted_at"`
-}
-
-func (Order) TableName() string {
-	return "online_shop.\"order\""
+	OrderItemGroupID int64          `gorm:"order_item_group_id"`
+	UserID           int64          `gorm:"user_id" gorm:"index"`
+	Amount           int64          `gorm:"amount"`
+	AmountWithoutTax int64          `gorm:"amount_without_tax"`
+	Tax              int64          `gorm:"tax"`
+	CreatedAt        time.Time      `gorm:"created_at"`
+	UpdatedAt        time.Time      `gorm:"updated_at"`
+	DeletedAt        gorm.DeletedAt `gorm:"deleted_at"`
 }
--- gorm/sample.sql
+++ gorm/sample.sql
@@ -1,10 +1,10 @@
 -- postgresql用のコードです。テーブルは思いつきで作ったやつなので本番利用しないで。
 create database myshop
 create schema online_shop;
-create table "order"
+create table "orders"
 (
     id                  bigserial
-        constraint order_pk
+        constraint orders_pk
             primary key,
     order_item_group_id bigserial,
     user_id             bigserial,
@@ -18,34 +18,31 @@ create table "order"
     why_deleted         text
 );
 
-comment on table "order" is '注文履歴';
+comment on table "orders" is '注文履歴';
 
-comment on column "order".order_item_group_id is '商品IDのリスト';
+comment on column "orders".order_item_group_id is '商品IDのリスト';
 
-comment on column "order".user_id is 'ユーザID';
+comment on column "orders".user_id is 'ユーザID';
 
-comment on column "order".amount is '税込価格(商品価格の合計)';
+comment on column "orders".amount is '税込価格(商品価格の合計)';
 
-comment on column "order".amount_without_tax is '税抜価格(税抜の商品価格の合計)';
+comment on column "orders".amount_without_tax is '税抜価格(税抜の商品価格の合計)';
 
-comment on column "order".tax is '消費税(商品価格に対する税の合計)';
+comment on column "orders".tax is '消費税(商品価格に対する税の合計)';
 
-comment on column "order".tax_rate_id is '消費税率ID';
+comment on column "orders".tax_rate_id is '消費税率ID';
 
-comment on column "order".why_deleted is '削除した理由';
+comment on column "orders".why_deleted is '削除した理由';
 
-alter table "order"
-    owner to "FJIO8880awz0";
+create index orders_user_id_index
+    on "orders" (user_id);
 
-create index order_user_id_index
-    on "order" (user_id);
+create index orders_user_id_orders_item_group_id_index
+    on "orders" (user_id, orders_item_group_id);
 
-create index order_order_item_group_id_index
-    on "order" (order_item_group_id);
+create index orders_user_id_id_index
+    on "orders" (user_id, id);
 
-create index order_user_id_order_item_group_id_index
-    on "order" (user_id, order_item_group_id);
-
-create index order_user_id_id_index
-    on "order" (user_id, id);
+create index orders_order_item_group_id_index
+    on "orders" (order_item_group_id);
 
